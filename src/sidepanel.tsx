9e826cc19ea9b46e6786774572f9e62c55b389e4
Caleb Fahlgren <calebfahlgren@protonmail.com>
2024-06-24 15:35:46 -0500

    init extension with tailwindcss + sidepanel

---
+++ src/sidepanel.tsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+function IndexSidePanel() {
+
+  return (
+    <div className="p-4 bg-white shadow-lg h-screen rounded-lg">
+      <h1 className="text-5xl font-bold text-gray-800">Side Panel</h1>
+    </div>
+  )
+}
+
+export default IndexSidePanel
