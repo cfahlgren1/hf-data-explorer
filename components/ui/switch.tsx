75e214c0ba2d173acdaaec6348b25d6b88b1fd3c
Caleb Fahlgren <calebfahlgren@protonmail.com>
2024-06-25 22:02:25 -0500

    add toggle for ui with local storage

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,7 +1,6 @@
-import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
-
 import { cn } from "@/lib/utils"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+import * as React from "react"
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
@@ -13,8 +12,7 @@ const Switch = React.forwardRef<
       className
     )}
     {...props}
-    ref={ref}
-  >
+    ref={ref}>
     <SwitchPrimitives.Thumb
       className={cn(
         "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
--- src/content.tsx
+++ src/content.tsx
@@ -4,69 +4,80 @@ import { useDuckDB } from "@/hooks/useDuckDB"
 import cssText from "data-text:~/styles.css"
 import type { PlasmoCSConfig } from "plasmo"
 import React, { useState } from "react"
+
+import { useStorage } from "@plasmohq/storage/hook"
+
 import "./styles.css"
+
 export const config: PlasmoCSConfig = {
-    matches: ["https://huggingface.co/datasets/*/*"]
+  matches: ["https://huggingface.co/datasets/*/*"]
 }
 
 export const getStyle = () => {
-    const style = document.createElement("style")
-    style.textContent = cssText
-    return style
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
 }
 
 const Content = () => {
-    const [query, setQuery] = useState<string>("")
-    const [results, setResults] = useState<any[]>([])
-    const [error, setError] = useState<string | null>(null)
+  const [showExplorer] = useStorage("showExplorer")
 
-    const { executeSQL, isQueryRunning, loading } = useDuckDB()
+  console.log("showExplorer", showExplorer)
 
-    const MAX_ROWS = 500
+  if (!showExplorer) return null
 
-    const runQuery = async () => {
-        // if query is empty or already running ignore
-        if (query.trim() === "" || isQueryRunning) return
-        setResults([])
-        setError(null)
-        try {
-            const { rows } = await executeSQL(query, MAX_ROWS)
-            setResults(rows)
-        } catch (err) {
-            setError(err.message)
-        }
+  return <Explorer />
+}
+
+const Explorer = () => {
+  const [query, setQuery] = useState<string>("")
+  const [results, setResults] = useState<any[]>([])
+  const [error, setError] = useState<string | null>(null)
+  const { executeSQL, isQueryRunning, loading } = useDuckDB()
+
+  const MAX_ROWS = 500
+
+  const runQuery = async () => {
+    // if query is empty or already running ignore
+    if (query.trim() === "" || isQueryRunning) return
+    setResults([])
+    setError(null)
+    try {
+      const { rows } = await executeSQL(query, MAX_ROWS)
+      setResults(rows)
+    } catch (err) {
+      setError(err.message)
     }
+  }
 
-    if (loading) return;
+  if (loading) return
 
-    return (
-        <div className="bg-white border border-slate-200 fixed bottom-10 left-10 w-80 rounded-lg shadow-lg z-50">
-            <div className="p-4">
-                <h1 className="text-xl font-bold text-slate-800 mb-1">Data Explorer</h1>
-                <p className="text-xs text-slate-600 mb-3">
-                    Write and execute SQL queries in the editor below.
-                </p>
-                <Textarea
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    placeholder="Enter your SQL query here..."
-                    className="w-full p-2 text-sm border border-slate-300 rounded resize-none h-24 mb-3"
-                />
-                <Button
-                    onClick={runQuery}
-                    className="w-full">
-                    {isQueryRunning ? "Running..." : "Run Query"}
-                </Button>
-                {error && <p className="text-xs text-red-500 mt-2">{error}</p>}
-                {results.length > 0 && (
-                    <p className="text-xs text-center text-gray-600 mt-2">
-                        Returned {results.length} rows
-                        {results.length === MAX_ROWS && " (maximum reached)"}
-                    </p>
-                )}
-            </div>
-        </div>
-    )
+  return (
+    <div className="bg-white border border-slate-200 fixed bottom-10 left-10 w-80 rounded-lg shadow-lg z-50">
+      <div className="p-4">
+        <h1 className="text-xl font-bold text-slate-800 mb-1">Data Explorer</h1>
+        <p className="text-xs text-slate-600 mb-3">
+          Write and execute SQL queries in the editor below.
+        </p>
+        <Textarea
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter your SQL query here..."
+          className="w-full p-2 text-sm border border-slate-300 rounded resize-none h-24 mb-3"
+        />
+        <Button onClick={runQuery} className="w-full">
+          {isQueryRunning ? "Running..." : "Run Query"}
+        </Button>
+        {error && <p className="text-xs text-red-500 mt-2">{error}</p>}
+        {results.length > 0 && (
+          <p className="text-xs text-center text-gray-600 mt-2">
+            Returned {results.length} rows
+            {results.length === MAX_ROWS && " (maximum reached)"}
+          </p>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Content
